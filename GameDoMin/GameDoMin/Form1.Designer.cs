fef8d189f29ed98bcc6960c3ca7774222e0d2378
Ngô Quang Việt <64864349+nqviet98@users.noreply.github.com>
2020-05-11 04:50:47 +0700

    .

---
+++ GameDoMin/GameDoMin/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace GameDoMin
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.paldomin = new System.Windows.Forms.Panel();
+            this.lbsomin = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // paldomin
+            // 
+            this.paldomin.Location = new System.Drawing.Point(12, 56);
+            this.paldomin.Name = "paldomin";
+            this.paldomin.Size = new System.Drawing.Size(299, 304);
+            this.paldomin.TabIndex = 0;
+            // 
+            // lbsomin
+            // 
+            this.lbsomin.AutoSize = true;
+            this.lbsomin.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbsomin.Location = new System.Drawing.Point(25, 9);
+            this.lbsomin.Name = "lbsomin";
+            this.lbsomin.Size = new System.Drawing.Size(30, 24);
+            this.lbsomin.TabIndex = 1;
+            this.lbsomin.Text = "00";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(323, 372);
+            this.Controls.Add(this.lbsomin);
+            this.Controls.Add(this.paldomin);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel paldomin;
+        private System.Windows.Forms.Label lbsomin;
+    }
+}
+
---
+++ GameDoMin/GameDoMin/Form1.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GameDoMin
+{
+    public partial class Form1 : Form
+    {
+        Button[,] btmin = new Button[10,10];
+        int control = 0;
+        int[,] a = new int[10, 10];
+        int somin = 20;
+        int so_o_mo = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int i = 0, j = 0;
+            int width = 30;
+            lbsomin.BorderStyle = BorderStyle.Fixed3D;
+            for (i = 0; i < 10; i++)
+            {
+                for(j=0; j < 10; j++)
+                {
+                    btmin[i, j] = new Button();
+                    btmin[i, j].Width = width;
+                    btmin[i, j].Height = width;
+                    btmin[i, j].Location = new Point(i * width, j * width);
+                    btmin[i, j].Text = "";
+                    btmin[i, j].Click += new EventHandler(bammin);
+                    paldomin.Controls.Add(btmin[i, j]);
+                    a[i, j] = 0;
+                }
+            }
+            int dem = 0;
+            while (dem < somin)
+            {
+                Random rnd = new Random();
+                i = rnd.Next(10);
+                j = rnd.Next(10);
+                if (a[i, j] == 0)
+                {
+                    a[i, j] = 1;
+                    dem++;
+                }
+            }
+        }
+        private void bammin(object sender, EventArgs e)
+        {
+            if(control == 0)
+            {
+                int width = 30;
+                int x = 0, y = 0;
+                x = ((Button)sender).Location.X;
+                y = ((Button)sender).Location.Y;
+                int i = 0, j = 0;
+                i = x / width;
+                j = y / width;
+                mo_o(i, j);
+                
+            }
+            if(control == 1)
+            {
+                MessageBoxButtons buttons = MessageBoxButtons.YesNo;
+                DialogResult result = MessageBox.Show("ban thang, ban co muon choi lai khong:", "Thong bao");
+            }
+            else
+                if(control == -1)
+            {
+               DialogResult dlr = MessageBox.Show("Ban thang, ban co muon choi lai khong?", "Thong bao", MessageBoxButtons.YesNo);
+                if(dlr == DialogResult.Yes)
+                {
+                    this.Hide();
+                    this.Close();
+                    this.ShowDialog();
+                }
+
+            }
+        }
+        private void mo_o(int i, int j)
+        {
+            if (a[i, j] == 0)
+            {
+                if (btmin[i,j].Text == "")
+                {
+                    so_o_mo++;
+                    if (sominxungquanh(i, j) == 0)
+                    {
+                        btmin[i, j].Text = " ";
+                        btmin[i, j].BackColor = Color.Beige;
+                        mo_o_lan_can(i, j);
+                    }
+                    else
+                        btmin[i, j].Text = sominxungquanh(i, j).ToString();
+                    lbsomin.Text = so_o_mo.ToString();
+                }
+            }
+            else
+            {
+                btmin[i, j].Text = "b";
+                btmin[i, j].BackColor = Color.Red;
+                control = -1;
+            }
+        }
+        private void mo_o_lan_can(int i, int j)
+        {
+            for (int u = i - 1; u <= i + 1; u++)
+            {
+                for (int v = j - 1; v <= j + 1; v++)
+                {
+                    if (u < 0 || u >= 10 || v < 0 || v >= 10)
+                    {
+                        continue;
+                    }
+                    if (u == i && v == j)
+                    {
+                        continue;
+                    }
+                    if (btmin[u,v].Text == "" && a[u,v] == 0)
+                    {
+                        so_o_mo++;
+                        mo_o(u, v);
+                    }
+                }
+            }
+        }
+        private void danhdau(object sender, MouseEventArgs e)
+        {
+
+        }
+        private int sominxungquanh(int i, int j)
+        {
+            int minxungquanh = 0;
+            for (int u = i - 1; u <= i + 1; u++)
+            {
+                for (int v = j - 1; v <= j + 1; v++)
+                {
+                    if (u < 0 || u >= 10 || v < 0 || v >= 10)
+                    {
+                        continue;
+                    }
+                    if (u == i && v == j)
+                    {
+                        continue;
+                    }
+                    minxungquanh += a[u, v];
+                }
+            }
+            return minxungquanh;
+        }
+
+    }
+}
